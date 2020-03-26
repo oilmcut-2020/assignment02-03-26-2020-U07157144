b3caedeea4abead1f42fdb0247e9ad6e1f2a3a60
U07157144 <61823441+U07157144@users.noreply.github.com>
2020-03-26 22:32:51 +0800

    Add files via upload

---
+++ Displayprimenumbers.java
@@ -0,0 +1,17 @@
+
+
+public class Displayprimenumbers {
+	public static void main(String[] args) {
+	 int i, j;
+        for (i = 1; i <= 100; i++) {
+             for (j = 2; j < i; j++) {
+                if (i % j == 0)
+                    break;
+             }
+            if (j >= i)
+                 System.out.println(i);
+         }
+    }
+}
+ 
+
